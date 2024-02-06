c44b536cf39836a9d8bdbb1e7460d8820b6ac35a
tajtanvvir1 <tanvirah1223@gmail.com>
2024-02-07 00:11:23 +0600

    task 3 done

--- Prb3.js
+++ Prb3.js
@@ -5,3 +5,23 @@
  * if they are equal then return as string 'equal'
  * if they are negative then return 'invalid input'
  */
+
+function sortMaker(arr) {
+   const [ a, b ] = arr;
+   if (a < 0 || b < 0) {
+      return "Invalid input"
+   }
+
+   if (a == b) {
+      return "equal"
+   }
+
+   if (a > b) {
+      return [a, b]
+   }
+   else {
+      return [b, a]
+   }
+}
+
+console.log(sortMaker([-5, 3]));

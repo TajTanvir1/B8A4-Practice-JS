cdf377d3da3c56f59dd5c67e70e99efcfd2bd9d7
tajtanvvir1 <tanvirah1223@gmail.com>
2024-02-06 23:12:38 +0600

    task 2 adding

--- Prb1.js
+++ Prb1.js
@@ -10,13 +10,15 @@
 
 //write cubeNumber() named function
 function cubeNumber(number){
-
+//function input will get only === number
     if (typeof number !== 'number') {
+        //if input is not a number then give a (Meaningful massage)
         return "Please enter a number"
     }
+    //result will return as cube (power)
     const result = Math.pow(number, 3);
 
     return result;
 }
 
-console.log(cubeNumber([]));
+console.log(cubeNumber(3));
---
+++ Prb2.js
@@ -0,0 +1,4 @@
+/**
+ * write a function matchFinder name
+ * if string1 is match by words with string2 then return true
+ */

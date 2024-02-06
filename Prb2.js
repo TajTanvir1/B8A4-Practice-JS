a25f0991b43b9af82a68fc932c97dcdb3fdfa158
tajtanvvir1 <tanvirah1223@gmail.com>
2024-02-06 23:24:06 +0600

    task 2 done

--- Prb2.js
+++ Prb2.js
@@ -1,4 +1,4 @@
-
+/**
  * write a function matchFinder name
  * if string1 is match by words with string2 then return true
  * if not match properly then return false
@@ -6,4 +6,12 @@
  * ---------------Bonus----------
  * check the input is a string
  * if they are not match then return a meaningful massage
- 
+ */
+
+function matchFinder(string1, string2){
+   const result = string1.includes(string2)
+   return result
+}
+
+console.log(matchFinder("John Doe","ohn"));
+console.log(matchFinder("Javascript","code"));

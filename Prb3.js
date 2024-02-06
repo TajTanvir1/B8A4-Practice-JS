9f3fb0f632a66ae81e7e179037b1465825a0de22
tajtanvvir1 <tanvirah1223@gmail.com>
2024-02-06 23:59:52 +0600

    Prb 3 add

---
+++ Prb3.js
@@ -0,0 +1,7 @@
+/**
+ * make a function name as sortMaker(), this will take an array as input
+ * access the element of the input array
+ * if the elements are positive then return array by large to small
+ * if they are equal then return as string 'equal'
+ * if they are negative then return 'invalid input'
+ */

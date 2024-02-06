97ff1ad9957edece00bd744a342f0b28d0b8e039
tajtanvvir1 <tanvirah1223@gmail.com>
2024-02-06 23:15:58 +0600

    task 2 add

--- Prb2.js
+++ Prb2.js
@@ -1,4 +1,9 @@
-/**
+
  * write a function matchFinder name
  * if string1 is match by words with string2 then return true
- */
+ * if not match properly then return false
+ * 
+ * ---------------Bonus----------
+ * check the input is a string
+ * if they are not match then return a meaningful massage
+ 

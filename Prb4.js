a103959031c352ca1b3daa0dd6f79fcc639a34c6
tajtanvvir1 <tanvirah1223@gmail.com>
2024-02-07 07:53:06 +0600

    Prb4 add

---
+++ Prb4.js
@@ -0,0 +1,7 @@
+/**
+ * create a function named as "findAddress()" which will take object
+ * access the elements of the object
+ * form the string as give sample"10,15A,Earth Perfect
+ * replace the missing property with underscore "__"
+ *  
+ */
---
+++ Prb5.js
@@ -0,0 +1,5 @@
+/**
+ * make a function named as canPay()
+ * function will take 2 parameter
+ * 
+ */
